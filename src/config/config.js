d879f76893d9eca956e55fd842f965f07dcdd146
Mohamed Abodia <mohamed.ibrahim32@msa.edu.eg>
2025-04-04 15:17:15 +0200

    Update Date

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,7 @@ const config = {
     brideName: "نوران",
     parentGroom: "والد العريس ووالدة العريس",
     parentBride: "والد العروس ووالدة العروس",
-    date: "2025-4-18",
+    date: "2025-04-18",
     maps_url: "https://maps.app.goo.gl/oorfKXJyZiL51JNV7",
     maps_embed:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3454.540235294004!2d31.364215599999994!3d30.021355399999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583ea25252c149%3A0x1c1f7b9c038b4034!2sRamage%20Hotel%2C%20Resort%2C%20And%20Spa!5e0!3m2!1sen!2seg!4v1743769476452!5m2!1sen!2seg",
@@ -19,7 +19,7 @@ const config = {
     agenda: [
       {
         title: "حفل الزفاف",
-        date: "2025-4-18",
+        date: "2025-04-18",
         startTime: "16:16",
         endTime: "17:30",
         location: "القاعة الكبرى، فندق رماج",
